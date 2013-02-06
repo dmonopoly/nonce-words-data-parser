1d657f8d4f0abca4fd0268eab78c259b7ca2b88b
David Zhang <dzhang21@gmail.com>
2013-02-06 12:11:32 -0800

    middle state of parsing function

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ nonce-words-data-parser
 =======================
 
 Simple parsing of data for a research project centered on nonce (nonsense) words. Uses a hosted site on gdrive.
+
+Assumes a decent web browser.
--- parser.js
+++ parser.js
@@ -1,39 +1,81 @@
+// Global var
+var DELIMITER = ",";
+
 // Returns the transformed text
 function transform(text) {
 	console.log("Input: \n"+text);
-	var result = text;
-	// var myText = ...; // Input text
-	// var lines = myText.split("\n");
-	// var numLines = lines.length;
-	// var i;
-	// var currentSection;
-	// var sections = Array();
-	// var phrases = Array();
 
-	// // parse phrases
-	// for (i = 0; i < numLines; i++) {
-	// 	var line = lines[i];
-	// 	if (line.indexOf('@') == 1) {
- //    		// start of e.g. time section, handled in nex loop
- //    		break;
-	// 	} else {
-	// 	    // phrase
-	// 	    phrase.push(line);
-	// 	}
-	// }
+	// Seed it
+	text = "Hi , this"; // tmp
 
-	// // parse sections
-	// for ( ; i < numLines; i++) {
-	// 	var line = lines[i];
-	// 	if (line.indexOf('@') == 1) {
-	// 	    // start of next section, handled in nex loop
-	//     	currentSection = line;
-	//     	sections[currentSection] = new Array();
-	// 	} else {
-	//     	// add section entry
-	//     	sections[currentSection].push(line);
-	// 	}
-	// }
+	var lines = text.split("\n");
+	var numLines = lines.length;
 
+	// resultant rows - 
+	// after algorithm rows[0] could be e.g. "stoofed, XX, 7, hugged, YY, 6, [XX-YY]" with XX and YY as RSAs
+	var rows = Array(); // can do rows.push(element)
+
+	for (var i=0; i<numLines; i++) {
+		var line = lines[i];
+		var inputRow = line.split(',');
+		// inputRow[0] -> nonce word
+		// inputRow[1] -> interpretation
+		_trim(inputRow);
+		console.log("Trimmed Output 2: ("+inputRow[0]+")("+inputRow[1]+")");
+		rows.push(_generateOutputRow(inputRow[0], inputRow[1]));
+	}
+
+	// test
+	// rows[0] = "stoofed, 5, 7, hugged, 4, 6, 1"
+	// rows[1] = "stoofed, 5, 7, hugged, 4, 6, 1"
+	// rows[2] = "stoofed, 5, 7, hugged, 4, 6, 1"
+	var result = _combineRows(rows);
 	return result;
+}
+
+// Generates a single output row based on the given nonce word and interpretation.
+function _generateOutputRow(nonceWord, interpretation) {
+	var output = Array();
+	var rsa1 = _computeRSA(nonceWord);
+	var rsa2 = _computeRSA(interpretation);
+
+	output[0] = nonceWord;
+	output[1] = rsa1;
+	output[2] = _countLetters(nonceWord);
+	output[3] = interpretation;
+	output[4] = rsa2;
+	output[5] = _countLetters(interpretation);
+	output[6] = rsa1 - rsa2;
+
+	// flatten to string first?
+	return output;
+}
+
+function _computeRSA(word) {
+	return '[RSA HERE]'
+}
+
+function _countLetters(word) {
+	return '[COUNT HERE]'
+}
+
+// Combines rows into a string. Used to produce resulting string.
+function _combineRows(rows) {
+	var result = ""; // a string
+	console.log("Combining rows");
+	for (var i=0; i<rows.length; i++) {
+		result += rows[i] + '\n';
+	}
+	return result;
+}
+
+// Trims each element of the given array myArray
+// JS _does_ change value directly
+// @param myArray - array of strings
+function _trim(myArray) {
+	console.log("Trimming");
+	for (var i=0; i<myArray.length; i++) {
+		myArray[i] = myArray[i].trim();
+	}
+	console.log("Trimmed Output 1: ("+myArray[0]+")("+myArray[1]+")");
 }
