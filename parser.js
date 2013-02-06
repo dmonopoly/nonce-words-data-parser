1283c473edb5797b91f8dc18303b5f32be6faf59
David Zhang <dzhang21@gmail.com>
2013-02-06 10:02:05 -0800

    first commit: site setup, parsing js setup

---
+++ parser.js
@@ -0,0 +1,39 @@
+// Returns the transformed text
+function transform(text) {
+	console.log("Input: \n"+text);
+	var result = text;
+	// var myText = ...; // Input text
+	// var lines = myText.split("\n");
+	// var numLines = lines.length;
+	// var i;
+	// var currentSection;
+	// var sections = Array();
+	// var phrases = Array();
+
+	// // parse phrases
+	// for (i = 0; i < numLines; i++) {
+	// 	var line = lines[i];
+	// 	if (line.indexOf('@') == 1) {
+ //    		// start of e.g. time section, handled in nex loop
+ //    		break;
+	// 	} else {
+	// 	    // phrase
+	// 	    phrase.push(line);
+	// 	}
+	// }
+
+	// // parse sections
+	// for ( ; i < numLines; i++) {
+	// 	var line = lines[i];
+	// 	if (line.indexOf('@') == 1) {
+	// 	    // start of next section, handled in nex loop
+	//     	currentSection = line;
+	//     	sections[currentSection] = new Array();
+	// 	} else {
+	//     	// add section entry
+	//     	sections[currentSection].push(line);
+	// 	}
+	// }
+
+	return result;
+}
