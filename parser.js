f67ef8ba05e11ba3da06b5e303a9829fbec24769
David Zhang <dzhang21@gmail.com>
2013-03-03 21:22:06 -0800

    Added LevDist calculator

--- parser.js
+++ parser.js
@@ -3,8 +3,35 @@ var DELIMITER = ",";
 var LEFT_LETTERS = ['q','w','e','r','t','a','s','d','f','g','z','x','c','v','b'];
 var RIGHT_LETTERS = ['y','u','i','o','p','h','j','k','l','n','m'];
 
+function transform1ColumnForLevDist(text) {
+	// console.log("Input: \n"+text);
+	text = text.trim();
+
+	var lines = text.split("\n");
+	var numLines = lines.length;
+
+	// resultant rows - 
+	// after algorithm rows[0] could be e.g. "stoofed, XX, 7" with XX as Lev Dist
+	var rows = Array(); // can do rows.push(element)
+
+	for (var i=0; i<numLines; i++) {
+		var line = lines[i];
+		var inputRow = line.split(',');
+		// inputRow[0] -> first word
+		// inputRow[1] -> second word
+		_trim(inputRow);
+		console.log("Trimmed Output 2: ("+inputRow[0]+")("+inputRow[1]+")");
+		rows.push(_generateLevDist(inputRow[0], inputRow[1]));
+	}
+	// test
+	// rows[0] = "stoofed"
+	// rows[1] = "stoofed"
+	var result = _flattenArrayByDelimiter(rows, '\n');
+	return result;
+}
+
 function transform1ColumnForRSAAndLength(text) {
-	console.log("Input: \n"+text);
+	// console.log("Input: \n"+text);
 	text = text.trim();
 
 	var lines = text.split("\n");
@@ -19,8 +46,8 @@ function transform1ColumnForRSAAndLength(text) {
 		var inputRow = line.split(',');
 		// inputRow[0] -> the only word
 		_trim(inputRow);
-		console.log("Trimmed Output 2: ("+inputRow[0]+")("+inputRow[1]+")");
-		rows.push(_generateOutputRowFromOneWord(inputRow[0]));
+		// console.log("Trimmed Output 2: ("+inputRow[0]+")("+inputRow[1]+")");
+		rows.push(_generateRSARowFromOneWord(inputRow[0]));
 	}
 	// test
 	// rows[0] = "stoofed"
@@ -31,7 +58,7 @@ function transform1ColumnForRSAAndLength(text) {
 
 // Returns the transformed text
 function transform2ColumnsForRSAAndLength(text) {
-	console.log("Input: \n"+text);
+	// console.log("Input: \n"+text);
 	text = text.trim();
 
 	var lines = text.split("\n");
@@ -47,8 +74,8 @@ function transform2ColumnsForRSAAndLength(text) {
 		// inputRow[0] -> nonce word
 		// inputRow[1] -> interpretation
 		_trim(inputRow);
-		console.log("Trimmed Output 2: ("+inputRow[0]+")("+inputRow[1]+")");
-		rows.push(_generateOutputRowFromTwoWords(inputRow[0], inputRow[1]));
+		// console.log("Trimmed Output 2: ("+inputRow[0]+")("+inputRow[1]+")");
+		rows.push(_generateRSARowFromTwoWords(inputRow[0], inputRow[1]));
 	}
 
 	// test
@@ -59,7 +86,18 @@ function transform2ColumnsForRSAAndLength(text) {
 	return result;
 }
 
-function _generateOutputRowFromOneWord(word) {
+function _generateLevDist(word1, word2) {
+	var output = Array();
+	var lev_dist = _computeLevDist(word1, word2);
+
+	output[0] = word1;
+	output[1] = word2;
+	output[2] = lev_dist;
+
+	return _flattenArrayByDelimiter(output, DELIMITER);
+}
+
+function _generateRSARowFromOneWord(word) {
 	var output = Array();
 	var rsa = _computeRSA(word);
 
@@ -71,7 +109,7 @@ function _generateOutputRowFromOneWord(word) {
 }
 
 // Generates a single output row based on the given 2 words: nonce word and interpretation.
-function _generateOutputRowFromTwoWords(nonceWord, interpretation) {
+function _generateRSARowFromTwoWords(nonceWord, interpretation) {
 	var output = Array();
 	var rsa1 = _computeRSA(nonceWord);
 	var rsa2 = _computeRSA(interpretation);
@@ -126,16 +164,69 @@ function _flattenArrayByDelimiter(array, delimiter) {
 // JS _does_ change value directly
 // @param myArray - array of strings
 function _trim(myArray) {
-	console.log("Trimming");
+	// console.log("Trimming");
 	for (var i=0; i<myArray.length; i++) {
 		myArray[i] = myArray[i].trim();
 	}
-	console.log("Trimmed Output 1: ("+myArray[0]+")("+myArray[1]+")");
+	// console.log("Trimmed Output 1: ("+myArray[0]+")("+myArray[1]+")");
 }
 
 // Return true if array contains the object
 function _include(arr, obj) {
     return (arr.indexOf(obj) != -1);
+}
+
+// From http://stackoverflow.com/questions/11919065/sort-an-array-by-the-levenshtein-distance-with-best-performance-in-javascript
+// @author James Westgate
+function _computeLevDist(s, t) {
+    var d = []; //2d matrix
+
+    // Step 1
+    var n = s.length;
+    var m = t.length;
+
+    if (n == 0) return m;
+    if (m == 0) return n;
+
+    //Create an array of arrays in javascript (a descending loop is quicker)
+    for (var i = n; i >= 0; i--) d[i] = [];
+
+    // Step 2
+    for (var i = n; i >= 0; i--) d[i][0] = i;
+    for (var j = m; j >= 0; j--) d[0][j] = j;
+
+    // Step 3
+    for (var i = 1; i <= n; i++) {
+        var s_i = s.charAt(i - 1);
+
+        // Step 4
+        for (var j = 1; j <= m; j++) {
+
+            //Check the jagged ld total so far
+            if (i == j && d[i][j] > 4) return n;
+
+            var t_j = t.charAt(j - 1);
+            var cost = (s_i == t_j) ? 0 : 1; // Step 5
+
+            //Calculate the minimum
+            var mi = d[i - 1][j] + 1;
+            var b = d[i][j - 1] + 1;
+            var c = d[i - 1][j - 1] + cost;
+
+            if (b < mi) mi = b;
+            if (c < mi) mi = c;
+
+            d[i][j] = mi; // Step 6
+
+            //Damerau transposition
+            if (i > 1 && j > 1 && s_i == t.charAt(j - 2) && s.charAt(i - 2) == t_j) {
+                d[i][j] = Math.min(d[i][j], d[i - 2][j - 2] + cost);
+            }
+        }
+    }
+
+    // Step 7
+    return d[n][m];
 }
 
 /* Test methods */
